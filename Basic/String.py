ba77e20776317099f9beb07521a2685f9f838341
Habibur Rahman <anuhimel@gmail.com>
2020-10-20 20:25:16 +0600

    simple arithmetic with numbers

--- Basic/String.py
+++ Basic/String.py
@@ -12,5 +12,19 @@ print(name.lower() + "\n")
 """Concat string"""
 first_name = "habibur"
 last_name = "rahman"
+age = 32
 print(first_name + " " + last_name)
+# int can't be concat with str
+print("Happy " + str(age) + "th Birthday!")
 print("Hello, " + name.title() + "!\n")
+
+
+"""Stripping String"""
+# This method only strip before a char start to last char
+raw_string = "  habibur rahman    "
+# rstrip for right side
+print(raw_string.rstrip())
+# lstrip for left side
+print(raw_string.lstrip())
+# just strip for both side
+print(raw_string.strip())
---
+++ Basic/number.py
@@ -0,0 +1,18 @@
+# int
+print(3 + 2)
+
+# float
+print(1.2 + 2.9000)
+print(0.1 + 0.2)
+
+# mul
+print(2 * 4)
+
+# div
+print(3 / 2)
+
+# rem
+print(3.1 % 2)
+
+# power
+print(2 ** 4)
