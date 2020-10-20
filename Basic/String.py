1ee9e4a69623dfd17b5f3374ac27f283ac65ed13
Habibur Rahman <anuhimel@gmail.com>
2020-10-20 19:58:51 +0600

    String

---
+++ Basic/String.py
@@ -0,0 +1,16 @@
+"""Declared a string in a variable"""
+name = "habibur rahman"
+
+"""Changing Case in a String without change value"""
+print(name.title())
+print(name + "\n")
+
+"""Convert Upper and Lower Case"""
+print(name.upper())
+print(name.lower() + "\n")
+
+"""Concat string"""
+first_name = "habibur"
+last_name = "rahman"
+print(first_name + " " + last_name)
+print("Hello, " + name.title() + "!\n")
