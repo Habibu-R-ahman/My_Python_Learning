ec77ad754a06581e027d85bdee2584c316d7f5d8
Habibur Rahman <anuhimel@gmail.com>
2020-10-21 06:52:58 +0600

    Tuples

---
+++ Basic/Tuples.py
@@ -0,0 +1,8 @@
+# As like List, Tuples can store date
+# But tuples are unchangeable!
+# So Think before you Store!
+
+# In tuples we use square brackets
+names = ("habib", "nazim", "sakib")
+print(names)
+print(names[0])
