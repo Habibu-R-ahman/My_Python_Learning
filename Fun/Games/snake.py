c0a5885a877ca20893c9bc9787146366e672924f
Habibur Rahman <anuhimel@gmail.com>
2021-01-13 21:56:52 +0600

    Updated

--- Fun/Games/snake.py
+++ Fun/Games/snake.py
@@ -11,21 +11,24 @@ red = (213, 50, 80)
 green = (0, 255, 0)
 blue = (50, 153, 213)
  
-dis_width = 800
-dis_height = 600
+dis_width = 600
+dis_height = 400
  
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by Edureka')
  
 clock = pygame.time.Clock()
-# game_over = False
  
 snake_block = 10
 snake_speed = 15
  
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
- 
+
+def Your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
+
 def our_snake(snake_block, snake_list):
     for x in snake_list:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
@@ -57,7 +60,7 @@ def gameLoop():
         while game_close == True:
             dis.fill(blue)
             message("You Lost! Press C-Play Again or Q-Quit", red)
- 
+            Your_score(Length_of_snake - 1)
             pygame.display.update()
  
             for event in pygame.event.get():
@@ -103,7 +106,8 @@ def gameLoop():
                 game_close = True
  
         our_snake(snake_block, snake_List)
- 
+        Your_score(Length_of_snake - 1)
+        
         pygame.display.update()
  
         if x1 == foodx and y1 == foody:
